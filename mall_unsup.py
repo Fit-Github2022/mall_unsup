a1fcb555699f734f1daa953533d42dea64d8a486
Fit-Github2022 <115910564+Fit-Github2022@users.noreply.github.com>
2022-10-23 13:51:11 +0800

    Update mall_unsup.py

--- mall_unsup.py
+++ mall_unsup.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import seaborn as sns
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
